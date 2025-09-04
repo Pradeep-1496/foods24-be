a3e22a6839021ff7eefe8f8b6392f4f3cf4687bb
pradeep-1496 <d23dit103@charusat.edu.in>
2025-09-04 20:05:11 +0530

    first

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
 // DB Connection
 // mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/foodapp');
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 connectDB();
 
 // Routes
-app.use('/auth/user', require('./routes/auth.user'));
-app.use('/auth/admin', require('./routes/auth.admin'));
-app.use('/auth/restaurant', require('./routes/auth.restaurant'));
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
+app.use("/auth/user", require("./routes/auth.user"));
+app.use("/auth/admin", require("./routes/auth.admin"));
+app.use("/auth/restaurant", require("./routes/auth.restaurant"));
 
 // add menu by restaurant
-app.use('/restaurant', require('./routes/menu.restaurant'));
-app.use('/restaurant', require('./routes/restaurant.routes'));
+app.use("/restaurant", require("./routes/menu.restaurant"));
+app.use("/restaurant", require("./routes/restaurant.routes"));
 
-
-// user order 
-app.use('/', require('./routes/order.routes'));
+// user order
+app.use("/", require("./routes/order.routes"));
 
 // add tables by restaurant
-app.use('/restaurant', require('./routes/table.routes'));
-
-
-
+app.use("/restaurant", require("./routes/table.routes"));
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`\n\n\n\t\t\t\tApplication listening on port ${port}`));
+app.listen(port, () =>
+  console.log(`\n\n\n\t\t\t\tApplication listening on port ${port}`)
+);
